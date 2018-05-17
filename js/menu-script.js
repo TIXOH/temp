f9bc4ffa96a15739c50e4176b45223fd88e91a01
TIXOH <30859201+TIXOH@users.noreply.github.com>
2018-05-18 00:57:12 +0300

    Add files via upload

--- js/menu-script.js
+++ js/menu-script.js
@@ -1,10 +1,10 @@
 var menuItem = document.querySelectorAll(".menu-item");
 
 for (var i = 0; i < menuItem.length; i++) {
-    menuItem[i].addEventListener("click", setActive);
+    menuItem[i].addEventListener("click", setMenuActive);
 }
 
-function setActive() {
+function setMenuActive() {
     for (var i = 0; i < menuItem.length; i++) {
         menuItem[i].classList.remove("menu-active");
     }
